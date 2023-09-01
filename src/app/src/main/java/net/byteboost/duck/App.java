a5d07a0296555dd0b7ae4b47cd56fb1c06a88b5d
MarkVN2 <markosvinicius.nunes@gmail.com>
2023-09-01 18:33:33 -0300

    [chore]: initial setup

---
+++ src/app/src/main/java/net/byteboost/duck/App.java
@@ -0,0 +1,8 @@
+
+package net.byteboost.duck;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Duck says 'Quack' to the World!");
+    }
+}
---
+++ src/app/src/test/java/duck/AppTest.java
@@ -0,0 +1,12 @@
+
+package duck;
+
+import net.byteboost.duck.App;
+import org.junit.jupiter.api.Test;
+class AppTest {
+
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+
+    }
+}
