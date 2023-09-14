e8faba22a3c3c98108022a172a1da53a63c4289c
Jaqueline Maria França Veloso da Silva <118978492+jaquemfvs@users.noreply.github.com>
2023-09-14 09:43:39 -0300

    Add Alterações
    Modificação do projeto.

--- src/app/src/main/java/net/byteboost/duck/gui/Apil.sql
+++ src/app/src/main/java/net/byteboost/duck/gui/Apil.sql
@@ -2,15 +2,15 @@ create database Log;
 
 use log;
 
-create table usuario
+create table user
 (id int primary key auto_increment,
 usuario varchar(30),
 senha varchar (10));
 
-select * from usuario;
+select * from user;
 
-insert into usuario (usuario,senha) values ('julia','123');
-insert into usuario (usuario,senha) values ('nini','123');
+insert into user (usuario,senha) values ('julia','123');
+insert into user (usuario,senha) values ('nini','123');
 
 
 
