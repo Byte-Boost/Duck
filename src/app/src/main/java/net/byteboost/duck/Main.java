9f001f6d20f0e6eb9024f5382c7252e7b2681fa2
BiaCapelo <bianca.silva135@fatec.sp.gov.br>
2023-09-15 07:48:44 -0300

    Login Page

--- src/app/src/main/java/net/byteboost/duck/Main.java
+++ src/app/src/main/java/net/byteboost/duck/Main.java
@@ -7,17 +7,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/aichat.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/fxml/login.fxml")));
 
-        primaryStage.setTitle("Test!");
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
+
         primaryStage.show();
+
 
     }
 }
