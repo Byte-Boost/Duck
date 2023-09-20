2fd814dcf42becafd7c4783ed6b78033184fd6f1
Marcos-Antonio-Rodrigues-dos-Santos <126624403+Marcos-Antonio-Rodrigues-dos-Santos@users.noreply.github.com>
2023-09-20 16:07:53 -0300

    [feat]: AI chat responses now show on chat box

--- src/app/src/main/java/net/byteboost/duck/gui/AiControllerChat.java
+++ src/app/src/main/java/net/byteboost/duck/gui/AiControllerChat.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import net.byteboost.duck.utils.AIutils;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -36,13 +37,21 @@ public class AiControllerChat implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 //Criação de labels pergunta no chat
+                chat.setSpacing(10);
                 Label question = new Label(tf_question.getText());
                 HBox hBoxQuestion = new HBox();
                 hBoxQuestion.getChildren().add(question);
                 hBoxQuestion.setAlignment(Pos.BASELINE_RIGHT);
                 hBoxQuestion.setStyle("-fx-padding:0 30 0 0");
                 chat.getChildren().add(hBoxQuestion);
-                chat.setSpacing(10);
+                //Criação de labels resposta do bot no chat
+                Label response = new Label(AIutils.loadIntoHugging(doc, tf_question.getText()));
+                HBox hBoxResponse = new HBox();
+                hBoxResponse.getChildren().add(response);
+                hBoxResponse.setAlignment(Pos.BASELINE_LEFT);
+                hBoxResponse.setStyle("-fx-padding:0 30 0 0");
+                chat.getChildren().add(hBoxResponse);
+
             }
         });
     }
